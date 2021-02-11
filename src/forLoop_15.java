ca478e88fd6b3e7eae97da73bb74275af5bdabb2
ZennT <zenn.diamond@gmail.com>
2021-02-10 16:50:05 -0800

    modified for loop

--- src/forLoop_15.java
+++ src/forLoop_15.java
@@ -1,2 +1,61 @@
-package PACKAGE_NAME;public class forLoop_15 {
+public class forLoop_15 {
+
+    public static void main(String[] args) {
+
+
+    /*
+    for(initialization;condition;incr/decr){
+    //statement or code to be executed
+    }
+
+     initialization : (executes once, only at the beginning) it is the initial condition which is executed once when the loop starts
+                      Here, we can initialize the variable, or we can use an already initialized variable.
+     condition : (executes each time) it is the second condition which is executed each time to test the condition
+                 of the loop. It continues execution until the condition is false.
+                 It must return boolean value either true or false. It is an optional condition.
+     statement : the statement of the loop is executed each time until the second condition is false.
+     increment / decrement : it increments or decrements the variable value. it is an optional condition.
+
+     */
+
+    // a for loop that will run 5 times
+    // i = i + 1 -> i++
+
+//        for(int i = 0; i < 5; i++ ){
+//            System.out.println("Hello");
+//        }
+
+
+    // the same with while loop
+//        int i = 0;
+//        while(i < 5){
+//            System.out.println("Hello");
+//            i++;
+//        }
+
+
+        // NESTED FOR LOOP
+
+//    for(int i = 0; i < 5; i++){
+//        for(int j =0; j < 2; j++){        // bu for loop bitmeden ustteki i yi arttirmaz
+//            System.out.println("i is: " + i + "j is: " + j); // outside forloop her calistiginda alttaki tekrar calisir
+//        }
+//    }
+
+
+//        for(int i = 0; i < 5;){   //i hic artmadigi ve 0 her zaman 5 den kucuk oldugu icin infinite loop
+//            System.out.println("Hello");  // condition her zaman true ise infinite loop a girer
+
+
+        for(;;){                    //infinite loop
+        System.out.println("***");
+        }
+
+
+
+
+
+    }
+
+
 }
