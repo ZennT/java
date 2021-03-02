6aa17dab7f90ed73dab2e905b2f31fdc7d4f58d4
ZennT <zenn.diamond@gmail.com>
2021-03-01 21:25:18 -0800

    Update

--- src/forLoop_15.java
+++ src/forLoop_15.java
@@ -22,7 +22,7 @@ public class forLoop_15 {
     // i = i + 1 -> i++
 
 //        for(int i = 0; i < 5; i++ ){
-//            System.out.println("Hello");
+//            StringSystem.out.println("Hello");
 //        }
 
 
