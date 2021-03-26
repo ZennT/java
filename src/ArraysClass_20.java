150c906d53c9eb2293454abbc97cd9bdd78f9d2c
ZennT <zenn.diamond@gmail.com>
2021-03-26 16:56:38 -0700

    update on AdvacedArrayExample

---
+++ src/ArraysClass_20.java
@@ -0,0 +1,57 @@
+import java.util.Arrays;
+
+public class ArraysClass_20 {
+
+    /**
+     * THE ARRAYS CLASS
+     * A class that has many static methods used with an array
+     * These methods can be used for:
+     *  - convert an array to String --> Arrays.toString(array);
+     *  - sort an array  --> Arrays.sort(array); -> (ascending order-from smallest to largest)
+     *                   --> Arrays.sort(array,fromIndex,toIndex); -> sadece istedigimiz araligi sort etmek icin
+     *                      ->fromIndex:4,toIndex:7 orneginde 4. indexden basla 7. indexe kadar diyoruz, 4 dahil,7 haric
+     *  - search an array -->
+     *  - compare arrays -->
+     *  - fill an array with specific value -->
+     *  - copy an array to another array
+     */
+
+    public static void main(String[] args) {
+
+        //Arrays.toString examples
+//        double[] myArray = {1.0,2.0,3.0,4.0};
+//        System.out.println(Arrays.toString(myArray));
+
+//        int[] myArray = {49,36,100,81};
+//        System.out.println(Arrays.toString(myArray));
+
+        //Arrays.sort examples
+//        int[] myArray = {4,2,9,3,10,0,-6};
+//        Arrays.sort(myArray);
+//        System.out.println(Arrays.toString(myArray));
+
+//        char[] myArray = {'a','z','f','b','x'}; // char tipinde sort yaparken karakterin unicode numarasina gore bakar
+//        Arrays.sort(myArray);
+//        System.out.println(Arrays.toString(myArray));
+
+//        int[] myArray = {49,36,100,81,-100,-20,0,4};
+//        System.out.println(Arrays.toString(myArray));
+//        Arrays.sort(myArray);
+//        System.out.println(Arrays.toString(myArray));
+
+//        int[] array = {4,-2,9,3,10,0,-8};
+//        Arrays.sort(array,4,7);
+//        System.out.println(Arrays.toString(array));
+
+//        int[] myArray = {49,36,100,81,-100,-20,0,4};
+//        System.out.println(Arrays.toString(myArray));
+//        Arrays.sort(myArray,2,5);
+//        System.out.println(Arrays.toString(myArray));
+
+
+
+
+
+
+    }
+}
