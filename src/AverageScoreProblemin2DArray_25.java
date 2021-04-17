e43c1cb2a77e6a376da57a6e5cd9f1a7f44dad0f
ZennT <zenn.diamond@gmail.com>
2021-04-17 05:49:05 -0700

    Added AverageScoreProblemin2DArray_25.java

---
+++ src/AverageScoreProblemin2DArray_25.java
@@ -0,0 +1,8 @@
+public class AverageScoreProblemin2DArray_25 {
+    /**
+     * Create a 2D array of three arrays in it (3 rows)
+     * Each row indicates the score of 4 classes (out of 100) that each student took
+     * Loop through the array, and print out the average score that each student got.
+     * (Try to use for each loop once)
+     */
+}
