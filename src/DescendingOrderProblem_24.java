b3323f92e44171fc3969037587529a954f0b52c1
ZennT <zenn.diamond@gmail.com>
2021-04-17 05:45:51 -0700

    Added DescendingOrderProblem_24.java

---
+++ src/DescendingOrderProblem_24.java
@@ -0,0 +1,11 @@
+public class DescendingOrderProblem_24 {
+    /**
+     * Ask from the user, how many numbers they want to sort
+     * (That should be the size of your array)
+     * Ask user to enter all the numbers they want to sort. Store them in an array you created
+     * Sort that array in a descending order (without using Arrays.sort method)
+     * Print the sorted array
+     *  Hint: 1) Use Scanner class to get user input
+     *        2) Use Arrays.copyOf to create a copy of an array to be sorted
+     */
+}
