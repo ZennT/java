7329aa7f4e1a7e76a7187556c3ca641db811e26b
ZennT <zenn.diamond@gmail.com>
2021-05-01 09:12:20 -0700

    added new class

--- src/string/StringExample.java
+++ src/string/StringExample.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class StringExample {
     public static void main(String[] args) {
+
 //        Scanner scanner = new Scanner(System.in);
 //        System.out.println("Please type in what size of coffee you want");
 //        System.out.println("Small, Medium or Large");
@@ -23,8 +24,29 @@ public class StringExample {
 //                break;
 //        }
 
-        String message = "I will see you at 8:00 AM";
-        System.out.println(message.toUpperCase());
+
+
+//        String message = "I will see you at 8:00 AM";
+//        System.out.println(message.toUpperCase());
+
+
+        String firstStr = "CodingBook";
+        String secondStr = "CodingBook";
+        String thirdStr = new String("CodingBook");
+
+        if(firstStr == secondStr){
+            System.out.println("firstStr = secondStr");
+        }else {
+            System.out.println("firstStr != secondStr");
+        }
+        if(firstStr.equals(thirdStr)){               // --> (firstStr == thirdStr) yazarsak esit degil sonucunu veriyor
+                                                     // cunku initialiation  farkli yapilmis. new kullanildiginda sadece deger atamiyor
+                                                     // new ile initialization yapilmissa, icindeki string degeri karsilastirabilmek icin  .equals kullanilmali
+            System.out.println("firstStr = thirdStr");
+        }else{
+            System.out.println("firstStr != thirdStr");
+        }
+
 
     }
 }
