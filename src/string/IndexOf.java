a1a1c999d6e767d92e802ede7344f81002e49c18
ZennT <zenn.diamond@gmail.com>
2021-05-04 00:31:34 -0700

    added IndexOf.java class

---
+++ src/string/IndexOf.java
@@ -0,0 +1,30 @@
+package string;
+
+/**
+ * Syntax: stringName.indexOf(int char);
+ * --> returns the index of the first appearance of the given char
+ * Syntax: stringName.indexOf(int char, int fromIndex);
+ * --> returns the index of the first appearance of the given char after specific index
+ * Syntax: stringName.indexOf(String anotherString);
+ * --> returns the index of the first appearance of the given string
+ * Syntax: stringName.indexOf(String anotherString, int fromIndex);
+ * --> returns the index of the first appearance of the given string after specific index
+ */
+
+public class IndexOf {
+    public static void main(String[] args) {
+//        String name = "You have successfully enrolled in CodingBook Java Path. We hope you all learn Java";
+//        System.out.println("u is located first at index: " + name.indexOf("u"));
+//        System.out.println("u is located first after 6 index at index: " + name.indexOf("u", 6  ));
+//        System.out.println("Java is located first at index: " + name.indexOf("Java"));
+//        System.out.println("Java is located first after 50 index at index: " + name.indexOf("Java", 50));
+
+
+        String name = "CodingBook";
+        System.out.println(name.indexOf("J"));  // returns -1
+
+
+
+
+    }
+}
