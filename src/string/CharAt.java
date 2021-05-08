cf6bf430d67853a47896e22b94c9a98acf5975e7
ZennT <zenn.diamond@gmail.com>
2021-05-07 21:54:44 -0700

    added CharAt.java

---
+++ src/string/CharAt.java
@@ -0,0 +1,14 @@
+package string;
+
+/**
+ * returns the characterthat is located at specific index
+ * Syntax: stringName.charAt(int index);
+ */
+
+public class CharAt {
+    public static void main(String[] args) {
+        String corona = "Coronavirus vaccine should come out before 2021.";
+        System.out.println("The caharacter located at index 6 is: " + corona.charAt(6));
+
+    }
+}
